7baf9c01c6a7aa7c36135374f45360e3bf7b8720
Srezzx <srieshimportant@gmail.com>
2021-05-08 12:00:28 +0530

    dashboard done

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,16 +1,24 @@
 import React from "react";
-import { Route, Redirect } from "react-router-dom";
-import QueueNumber from "../components/Dashboard/QueueNumber";
-import QueueTable from "../components/Dashboard/QueueTable";
+// import { Route, Redirect } from "react-router-dom";
+import QueueNumber from "../components/Dashboard/QueueNumber.js";
+import QueueTable from "../components/Dashboard/QueueTable.jsx";
 import Navbar from "../components/Dashboard/Navbar";
-import { Alert } from "reactstrap";
+import QueueDetails from "../components/Dashboard/QueueDetails.jsx";
+import Footer from "../components/Dashboard/Footer.jsx";
 
-const Dashboard = ({}) => {
+const Dashboard = () => {
   return (
     <div>
       <Navbar />
-      <QueueNumber />
-      <QueueTable />
+      <div className="container" >
+        <h5 className="fontstyle1 mt-5 ml-2">
+          Good Morning, <br></br>Sriesh
+        </h5>
+        <QueueNumber />
+        <QueueDetails />
+        <QueueTable />
+      </div>
+      <Footer />
     </div>
   );
 };
